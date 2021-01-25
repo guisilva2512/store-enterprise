3f86410fd2d01275ee491ff6c303d494b7ac1da4
Guilherme Silva <guilherme.aparecido@itenabler.com.br>
2021-01-25 20:24:13 -0300

    Comportamentos básicos API Catalogo

---
+++ src/building blocks/NSE.Core/Data/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NSE.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        
+    }
+
+    public interface IAggregateRoot { }
+}
---
+++ src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+
+        public ProdutoRepository(CatalogoContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _context.Produto.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _context.Produto.AsNoTracking().ToListAsync();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _context.Produto.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _context.Produto.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
