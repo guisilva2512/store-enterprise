c058d644d512c735e336e0c7d4cc825af2e3bf84
Guilherme Silva <guilherme.aparecido@itenabler.com.br>
2021-02-09 20:52:19 -0300

    Utilizando Refit para o consumo de API's

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,21 +1,35 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using System;
 
 namespace NSE.WebApp.MVC.Configuration
 {
     public static class DependencyInjectionConfig
     {
-        public static void RegisterService(this IServiceCollection services)
+        public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
 
             // IHttpFactory
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
+            #region Refit
+
+            services.AddHttpClient("Refit",
+                    options =>
+                    {
+                        options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+                    })
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+
+            #endregion
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
